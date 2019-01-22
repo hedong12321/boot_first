33f1551241c2d0a67f62b81c24b809385637f690
hed <comeonhd@sina.com>
2019-01-23 00:04:51 +0800

    lettuce, websocket

--- src/main/java/com/hed/boot/web/LoginController.java
+++ src/main/java/com/hed/boot/web/LoginController.java
@@ -1,14 +1,18 @@
 package com.hed.boot.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
+    @Value("${name}")
+    private String name;
+
     @RequestMapping("/doLogin")
     public String doLogin() {
-        return "welcome use this system!";
+        return "welcome use this system!" + " your name is " + name + "...";
     }
 
 }
